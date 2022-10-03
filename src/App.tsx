60f7f9f6a8f2dd964b2f31b848d3d3d70f0bd9e6
hikkama <khasanov.nurlan@gmail.com>
2022-10-03 14:21:56 +0600

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      Ывфв
-    </div>
-  );
+  return <div>Ывфв</div>
 }
 
-export default App;
+export default App
